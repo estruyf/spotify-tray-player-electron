fdb93004ab243949464b60f8328ff860fcff2817
Elio Struyf <info@estruyf.be>
2020-05-06 15:11:52 +0200

    Added song title + next song

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { app, Menu, Tray, nativeImage, NativeImage, MenuItemConstructorOptions } from 'electron';
+import Store from 'electron-store';
 import * as path from 'path';
 import { Spotify } from './services';
 
+const appName = 'spotify.tray.player';
 const assetsDirectory = path.join(__dirname, '../assets');
 
 const createImage = (fileName: string): NativeImage => {
@@ -12,36 +14,84 @@ const createImage = (fileName: string): NativeImage => {
 
 app.on('ready', () => {
   let playTimeout: NodeJS.Timeout = null;
+  let store = new Store();
   const tray = new Tray(createImage('spotify.png'));
 
   const menuItems: MenuItemConstructorOptions[] = [{ label: 'Quit', click: () => app.quit() }];
 
   tray.setToolTip('Spotify tray player');
 
+  /**
+   * Toggle player
+   */
   const toggle = async () => {
     await Spotify.toggle();
     setIcon();
   };
 
+  /**
+   * Play the next song
+   */
+  const next = async () => {
+    await Spotify.next();
+    setIcon();
+  }
+
+  /**
+   * Set the tray icon
+   */
   const setIcon = () => {
     setTimeout(async () => {
       const song = await Spotify.getState();
       if (song && song.state === "playing") {
         tray.setImage(createImage('music.png'));
+        if (getTrayTitleEnabled()) {
+          const track = await Spotify.getTrack();
+          if (track && track.name && track.artist) {
+            tray.setTitle(`  ${track.artist} - ${track.name}`);
+            return
+          }
+        }
+        
+        tray.setTitle("");
       } else {
         tray.setImage(createImage('spotify.png'));
+        tray.setTitle("");
       }
     }, 500);
   };
 
+  /**
+   * Schedule the background polling
+   */
   const scheduler = () => {
     setIcon();
 
+    const titleEnabled = getTrayTitleEnabled();
+
     setTimeout(() => {
       scheduler();
-    }, 1 * 60 * 1000); // Every minute
+    }, 1 * (titleEnabled ? 10 : 60) * 1000); // every 10 or 60 seconds
   };
 
+  /**
+   * Returns if the tray title is enabled
+   */
+  const getTrayTitleEnabled = () => {
+    return store.get(`${appName}.title.enabled`) || false;
+  }
+
+  /**
+   * Toggle the if the song title needs to be shown
+   */
+  const toggleSongTitle = () => {
+    store.set(`${appName}.title.enabled`, !getTrayTitleEnabled());
+    setIcon();
+  }
+
+  /**
+   * Single click event
+   */
   tray.on('click', async (event) => {
     // For capturing double click
     playTimeout = setTimeout(() => {
@@ -49,12 +99,18 @@ app.on('ready', () => {
     }, 100);
   });
 
-  tray.on('double-click', async (event) => {
+  /**
+   * Double click event
+   */
+  tray.on('double-click', (event) => {
     clearTimeout(playTimeout);
     playTimeout = null;
-    await Spotify.next();
+    next();
   });
 
+  /**
+   * Right click event
+   */
   tray.on('right-click', async (event) => {
     const track = await Spotify.getTrack();
     const song = await Spotify.getState();
@@ -71,11 +127,16 @@ app.on('ready', () => {
 
     if (song) {
       ctxMenuItems.push({ icon: song.state === "playing" ? createImage('pause.png') : createImage('play.png'), label: `   ${song.state === "playing" ? "Pause" : "Play"}`, click: toggle });
+      ctxMenuItems.push({ icon: createImage('next.png'), label: `   Next song`, click: next });
     }
 
     if (ctxMenuItems.length > 0) {
       ctxMenuItems.push({ type: 'separator' });
     }
+
+    ctxMenuItems.push({ label: getTrayTitleEnabled() ? 'Hide song title' : 'Show song title', click: toggleSongTitle });
+
+    ctxMenuItems.push({ type: 'separator' });
     
     const ctxMenu = Menu.buildFromTemplate([...ctxMenuItems, ...menuItems]);
     tray.popUpContextMenu(ctxMenu);
